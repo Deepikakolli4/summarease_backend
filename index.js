4c4f2baaaa48eb5b3a07ed2f38527e69d5cc39e6
Deepikakolli4 <deepikakolli2004@gmail.com>
2025-02-24 18:33:35 +0530

    added backend
    Signed-off-by: Deepikakolli4 <deepikakolli2004@gmail.com>

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const { getTranscript } = require('./controllers/authController');
+
+const app = express();
+
+app.use(cors({
+    origin: true,
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type']
+}));
+
+
+app.use(bodyParser.json());
+
+// Define routes
+app.post('/hello', (req, res) => {
+    res.send('hello');
+});
+
+app.post('/transcript', getTranscript);
+
+const PORT = 8000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
